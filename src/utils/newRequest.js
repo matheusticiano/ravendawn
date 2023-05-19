affda2e0e8c49c6f4931b6ca6fe18fb70e90010a
matheusticiano <matheusticiano34@gmail.com>
2023-05-19 18:47:22 -0300

    a

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://ravendawn.onrender.com/api/",
   withCredentials: true,
 });
 
