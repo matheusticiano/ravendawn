d41058c60d9b86f7cd676a5b15dcf1f3af4e9da5
matheusticiano <matheusticiano34@gmail.com>
2023-05-21 10:00:26 -0300

    sad

--- src/components/HuntCard.jsx
+++ src/components/HuntCard.jsx
@@ -8,29 +8,31 @@ const HuntCard = ({ hunt, location, level, image, profit, xp, huntId, creator, d
     return (
         <div className="hunt-card">
             <div className='hunt-info'>
-            <img src={image} alt={hunt} />
-            <h9>{hunt}</h9>
-            <div className='card-info'>
-                <div className='borda-direita'>
-                    <p9><strong>Location:</strong> {location}</p9>
+                <img src={image} alt={hunt} />
+                <h9>{hunt}</h9>
+                <div className='card-info'>
+                    <div className='borda-direita'>
+                        <p9><strong>Location:</strong> {location}</p9>
+                    </div>
+                    <p9><strong>Level:</strong> {level}</p9>
+                    <div className='borda-direita'>
+                        <p9><strong>Profit Por Hora:</strong> {profit}</p9>
+                    </div>
+                    <p9><strong>Xp Por Hora:</strong> {xp}</p9>
                 </div>
-                <p9><strong>Level:</strong> {level}</p9>
-                <div className='borda-direita'>
-                    <p9><strong>Profit Por Hora:</strong> {profit}</p9>
+                <div className='hunt-creator'>
+                    <p9><strong>Created by: {creator}</strong></p9>
+                    <p9>{data}</p9>
                 </div>
-                <p9><strong>Xp Por Hora:</strong> {xp}</p9>
-                <p9><strong>Created by: {creator}</strong></p9>
-                <p9>{data}</p9>
-            </div>
-            <div className='card-buttons'>
-                <LikeButton huntId={huntId} />
-                <SaveHunt huntId={huntId} />
-            </div>
-            {showMenu && (
-                <div className='delete-button'>
-                    <DeleteHunt huntId={huntId} />
+                <div className='card-buttons'>
+                    <LikeButton huntId={huntId} />
+                    <SaveHunt huntId={huntId} />
                 </div>
-            )}
+                {showMenu && (
+                    <div className='delete-button'>
+                        <DeleteHunt huntId={huntId} />
+                    </div>
+                )}
             </div>
         </div>
     );
